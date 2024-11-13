fc824c014511c61c4f0dddb23930ae94146ddcdc
seppou <sauvalle.seppou@groupr-esigelec.org>
2024-11-13 12:04:00 +0100

    ghgj

---
+++ acceuil1.php
@@ -0,0 +1,23 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - ESIGELEC Running</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+
+    <main>
+        <h1>Bienvenue sur le site de Running de l'ESIGELEC</h1>
+        <p>Rejoignez nos sessions de running et progressez ensemble !</p>
+        <div class="buttons">
+            <a href="pages/register.php" class="btn">S'inscrire</a>
+            <a href="pages/login.php" class="btn">Se connecter</a>
+        </div>
+    </main>
+
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
